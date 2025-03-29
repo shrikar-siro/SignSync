5f38507753677871b89f66349615b8c5d2fde71a
shrikar-siro <siro.shrikar@gmail.com>
2025-03-29 13:52:34 -0400

    add base UI for app

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [artist, setArtist] = useState("");
+
+  async function handleSearch() {
+    //add code here for handling search.
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="app w-75 mx-auto row justify-content-center">
+        <p className="h1 p-1">SongSync</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="container-fluid">
+        <div className="input-group">
+          <input
+            className="form-control"
+            type="text"
+            placeholder="Search for Artist..."
+            onChange={(e) => setArtist(e.target.value)}
+          ></input>
+          <button className="btn btn-primary" onClick={handleSearch}>
+            Search
+          </button>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
