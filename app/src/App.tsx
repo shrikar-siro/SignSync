515fb24afc3a10adb0229b0c1b668a2144db7e8f
shrikar-siro <siro.shrikar@gmail.com>
2025-03-29 17:52:56 -0400

    trying to get authentication to work, and data to print - not working

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,21 +1,46 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [count, setCount] = useState(0);
   const [artist, setArtist] = useState("");
 
-  async function handleSearch() {
+  //keeping credentials here - we can move them into ENV later.
+  const accessToken = import.meta.env.VITE_GENIUS_API_TOKEN;
+  // console.log(import.meta.env);
+  // console.log(accessToken);
+
+  async function artistSearch() {
     //add code here for handling search.
+    //Search URL:
+    const URL = "https://api.genius.com/search";
+
+    try {
+      const response = await fetch(`${URL}?q=${encodeURIComponent(artist)}`, {
+        method: "GET",
+        mode: "no-cors",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json",
+        },
+      });
+
+      //print data to find out what to access.
+      const dataAsText = await response.text();
+      if (!dataAsText) {
+        console.log("No data in the response!");
+      }
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log("Error in retreiving data. Error is: ", error);
+    }
   }
 
   return (
     <>
       <div className="app w-75 mx-auto row justify-content-center">
-        <p className="h1 p-1">SongSync</p>
+        <p className="h1 p-1 mb-3">SongSync</p>
       </div>
       <div className="container-fluid">
         <div className="input-group">
@@ -25,7 +50,7 @@ function App() {
             placeholder="Search for Artist..."
             onChange={(e) => setArtist(e.target.value)}
           ></input>
-          <button className="btn btn-primary" onClick={handleSearch}>
+          <button className="btn btn-primary" onClick={artistSearch}>
             Search
           </button>
         </div>
